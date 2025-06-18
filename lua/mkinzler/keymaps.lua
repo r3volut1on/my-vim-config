b92a0cec2cdff1ae2673315c3cbf7a533d5bd807
r3volut1on <mattkinz96@gmail.com>
2025-06-17 23:47:05 -0400

    Added some quality of life remaps

--- lua/mkinzler/keymaps.lua
+++ lua/mkinzler/keymaps.lua
@@ -2,3 +2,11 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Makes it so that jumping between pages keeps cursor in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Makes it so that jumping between searches keeps cursor in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
